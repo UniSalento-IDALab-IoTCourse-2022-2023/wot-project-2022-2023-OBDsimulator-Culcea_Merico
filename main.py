ae1f29038397a1a737e534b6361505d6af1d81b6
Steve <78929248+vetsen@users.noreply.github.com>
2023-06-14 13:52:52 +0200

    Timestamp + web socket
    Adesso il server invia anche il timestamp e fornisce i dati anche tramite web socket

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from threading import Thread, Lock
 import asyncio
-from time import sleep
+from time import sleep, time
+from websockets.server import serve
+import json
 from bless import (  
         BlessServer,
         BlessGATTCharacteristic,
         GATTCharacteristicProperties,
         GATTAttributePermissions
         )
+
 
 # define an enum-like, to avoid typing the string explicitly
 speed = "speed"
@@ -57,8 +60,9 @@ def gatt_server(lock, loop):
     def read_request(characteristic):
         # str(characteristic) is like: "51ff12bb-3ed8-46e5-b4f9-d64e2fec021b: Unknown"
         characteristic_name = characteristic_uuid_to_name[str(characteristic).split(":")[0].lower()]
-        data = obd_data[characteristic_name]
-        return bytearray(str(data).encode())
+        value = obd_data[characteristic_name]
+        data_to_send = str(time()) + "--" + str(value)
+        return bytearray(str(data_to_send).encode())
 
     async def run(loop):
         server = BlessServer(name="obd simulator", loop=loop)
@@ -74,15 +78,32 @@ def gatt_server(lock, loop):
 
     loop.run_until_complete(run(loop))
 
+def ws_server(lock, address, port):
+    async def send_data(websocket):
+        while True:
+            data_to_send = obd_data.copy()
+            data_to_send["timestamp"] = time()
+            json_result = json.dumps(data_to_send)
+            await websocket.send(json_result)
+            sleep(2)
+
+    async def ws():
+        async with serve(send_data, address, port):
+            await asyncio.Future()  
+
+    asyncio.run(ws())
+
 
 loop = asyncio.get_event_loop()
 
 lock = Lock()
 simulator_thread = Thread(target=simulate_data, args=(lock,))
 gatt_client_thread = Thread(target=gatt_server, args=(lock,loop))
+ws_server_thread = Thread(target=ws_server, args=(lock, "192.168.1.50", 8765))
 
 simulator_thread.start()
 gatt_client_thread.start()
+ws_server_thread.start()
 
 # end with ctrl+c
 
